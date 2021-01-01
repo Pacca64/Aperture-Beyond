94fbbcce984b692032b7c1aa45981b751e730a43
Pacguy64 <pacmax510@gmail.com>
2021-01-01 15:26:43 +0000

    finished basic prop_glados_core behavior

--- sp/src/game/server/prop_glados_core.cpp
+++ sp/src/game/server/prop_glados_core.cpp
@@ -2,10 +2,19 @@
 #include "props.h"
 #include <string>
 #include "prop_glados_core.h"
+#include "engine/IEngineSound.h"
+
 
 BEGIN_DATADESC(CPropGladosCore)
 
-DEFINE_KEYFIELD(m_speechDelay, FIELD_INTEGER, "DelayBetweenLines"),
+DEFINE_FIELD(m_isActive, FIELD_BOOLEAN),
+DEFINE_FIELD(m_isTurning, FIELD_BOOLEAN),
+DEFINE_FIELD(m_animJustEnded, FIELD_BOOLEAN),
+
+DEFINE_FIELD(m_voiceLineDuration, FIELD_FLOAT),
+DEFINE_FIELD(m_lastVoiceLineTime, FIELD_FLOAT),
+
+DEFINE_KEYFIELD(m_speechDelay, FIELD_FLOAT, "DelayBetweenLines"),
 DEFINE_KEYFIELD(m_CoreType, FIELD_INTEGER, "CoreType"),
 
 // Declare our think function
@@ -34,6 +43,12 @@ void CPropGladosCore::Precache(void)
 	PrecacheModel(pModelName);
 	SetModel(pModelName);
 
+	//PrecacheScriptSound("Portal.Glados_core.Curiosity_1");
+
+	precacheCoreVoiceLines(CORE_SOUNDSCRIPTNAME_CURIOUSITY, 17, false);
+	precacheCoreVoiceLines(CORE_SOUNDSCRIPTNAME_ANGER, 21, true);
+	precacheCoreVoiceLines(CORE_SOUNDSCRIPTNAME_CAKE, 41, true);
+
 	BaseClass::Precache();
 }
 
@@ -52,37 +67,267 @@ void CPropGladosCore::Spawn()
 
 	if (VPhysicsInitNormal(SOLID_VPHYSICS, 0, false) == NULL)
 	{
-		DevMsg("npc_personality_core unable to spawn physics object!\n");
+		DevMsg("prop_glados_core unable to spawn physics object!\n");
 	}
 
 	ResetSequence(LookupSequence("drop"));
 
 	switch (m_CoreType){
 
-	case CORESKIN_MORALITY:
+	case CORETYPE_MORALITY:
 		m_nSkin = CORESKIN_MORALITY;
 		break;
 
-	case CORESKIN_CURIOUSITY:
+	case CORETYPE_CURIOUSITY:
 		m_nSkin = CORESKIN_CURIOUSITY;
 		break;
 
-	case CORESKIN_ANGER:
+	case CORETYPE_ANGER:
 		m_nSkin = CORESKIN_ANGER;
 		break;
 
-	case CORESKIN_CAKE:
+	case CORETYPE_CAKE:
 		m_nSkin = CORESKIN_CAKE;
 		break;
 	}
+
+	m_isActive = false;
+	m_isTurning = false;
+
+	//initialize arrays of voice line durations
+	setupCoreVoiceLineDurations(voiceLineDurationsCurious, std::string(CORE_SOUNDFILENAME_CURIOUSITY), 18);
+	setupCoreVoiceLineDurations(voiceLineDurationsAnger, std::string(CORE_SOUNDFILENAME_ANGER), 21);
+	setupCoreVoiceLineDurations(voiceLineDurationsCake, std::string(CORE_SOUNDFILENAME_CAKE), 41);
 
 	SetThink(&CPropGladosCore::Think);
 	SetNextThink(gpGlobals->curtime + 0.1f);
 }
 
 void CPropGladosCore::Think(void){
-	BaseClass::AnimThink();
+	AnimThink();
+
+	//This if activates after the turn animation has ended.
+	//It sets each cores special animation, and toggles the isActive flag on.
+	if (m_isTurning == true && m_animJustEnded == true && m_isActive == false){
+		m_isTurning = false;
+		m_isActive = true;
+
+		switch (m_CoreType){
+
+		case CORETYPE_MORALITY:
+			PropSetAnim("idle");
+			m_iszDefaultAnim = castable_string_t("idle");
+			break;
+
+		case CORETYPE_CURIOUSITY:
+			PropSetAnim("look_02");
+			m_iszDefaultAnim = castable_string_t("look_02");
+			break;
+
+		case CORETYPE_ANGER:
+			PropSetAnim("look_03");
+			m_iszDefaultAnim = castable_string_t("look_03");
+			break;
+
+		case CORETYPE_CAKE:
+			PropSetAnim("look_04");
+			m_iszDefaultAnim = castable_string_t("look_04");
+			break;
+		}
+	}
+
+	//If we are active, and last voice line time + voice line delay is less then current time, AND we are not the morality core
+	if (m_isActive && (gpGlobals->curtime) >= m_lastVoiceLineTime + m_speechDelay + m_voiceLineDuration && m_CoreType != CORETYPE_MORALITY){
+		
+		//setup core name string variable
+		std::string coreNameString;
+
+		//Setup bool for whether file has a leading zero
+		bool hasLeadingZero = true;
+
+		//setup for each vocal core type. Resets voiceLineNumber, save correct line duration, and sets core name string.
+		if (m_CoreType == CORETYPE_CURIOUSITY){
+			coreNameString = CORE_SOUNDSCRIPTNAME_CURIOUSITY;
+
+			if (m_voiceLineNumber >= 17){
+				m_voiceLineNumber = 0;
+			}
+			//The curiousity cores voice lines are strange
+			//line 14 is missing, and line 15 is a panic input line, not normal speech.
+			//keep in mind that the code uses voice line number - 1
+			else if (m_voiceLineNumber == 13){
+				m_voiceLineNumber = 15;
+			}
+
+			hasLeadingZero = false;
+			m_voiceLineDuration = voiceLineDurationsCurious[m_voiceLineNumber];
+		}
+		else if (m_CoreType == CORETYPE_ANGER){
+			coreNameString = CORE_SOUNDSCRIPTNAME_ANGER;
+
+			if (m_voiceLineNumber >= 21){
+				m_voiceLineNumber = 0;
+			}
+
+			hasLeadingZero = true;
+			m_voiceLineDuration = voiceLineDurationsAnger[m_voiceLineNumber];
+		}
+		else if (m_CoreType == CORETYPE_CAKE){
+			coreNameString = CORE_SOUNDSCRIPTNAME_CAKE;
+
+			if (m_voiceLineNumber >= 41){
+				m_voiceLineNumber = 0;
+			}
+
+			hasLeadingZero = true;
+			m_voiceLineDuration = voiceLineDurationsCake[m_voiceLineNumber];
+		}
+
+		//play the appropriate voice line based on correct core name string.
+		playCoreVoiceLine(coreNameString, m_voiceLineNumber, hasLeadingZero);
+		m_voiceLineNumber++;	//increment voice line number
+
+		//save the time we played this line at to help time the next one.
+		m_lastVoiceLineTime = gpGlobals->curtime;
+	}
 
 	// Think at 20Hz
 	SetNextThink(gpGlobals->curtime + 0.05f);
+}
+
+//when player or physgun picks this up, do something
+void CPropGladosCore::OnPhysGunPickup(CBasePlayer *pPhysGunUser, PhysGunPickup_t reason){
+	if (m_isActive == false && m_isTurning == false){
+		PropSetAnim("turn");
+		m_isTurning = true;
+	}
+}
+
+//------------------------------------------------------------------------------
+// Purpose: Copied from CDynamicProp. Modified to do things when the turn animation is over
+//------------------------------------------------------------------------------
+void CPropGladosCore::AnimThink(void)
+{
+	m_animJustEnded = false;
+
+	if (m_nPendingSequence != -1)
+	{
+		FinishSetSequence(m_nPendingSequence);
+		m_nPendingSequence = -1;
+	}
+
+	if (m_bRandomAnimator && m_flNextRandAnim < gpGlobals->curtime)
+	{
+		ResetSequence(SelectWeightedSequence(ACT_IDLE));
+		ResetClientsideFrame();
+
+		// Fire output
+		m_pOutputAnimBegun.FireOutput(NULL, this);
+
+		m_flNextRandAnim = gpGlobals->curtime + random->RandomFloat(m_flMinRandAnimTime, m_flMaxRandAnimTime);
+	}
+
+	if (((m_iTransitionDirection > 0 && GetCycle() >= 0.999f) || (m_iTransitionDirection < 0 && GetCycle() <= 0.0f)) && !SequenceLoops())
+	{
+		Assert(m_iGoalSequence >= 0);
+		if (GetSequence() != m_iGoalSequence)
+		{
+			PropSetSequence(m_iGoalSequence);
+		}
+		else
+		{
+			// Fire output
+			m_pOutputAnimOver.FireOutput(NULL, this);
+			m_animJustEnded = true;
+
+			// If I'm a random animator, think again when it's time to change sequence
+			if (m_bRandomAnimator)
+			{
+				SetNextThink(gpGlobals->curtime + m_flNextRandAnim + 0.1);
+			}
+			else
+			{
+				if (m_iszDefaultAnim != NULL_STRING)
+				{
+					PropSetAnim(STRING(m_iszDefaultAnim));
+				}
+			}
+		}
+	}
+	else
+	{
+		SetNextThink(gpGlobals->curtime + 0.1f);
+	}
+
+	StudioFrameAdvance();
+	DispatchAnimEvents(this);
+	m_BoneFollowerManager.UpdateBoneFollowers(this);
+}
+
+//fills arrays for durations of voice lines
+void CPropGladosCore::setupCoreVoiceLineDurations(float voiceLineDurations[], std::string coreNameString, int numOfLines){
+	for (int i = 0; i < numOfLines; i++){
+
+		std::string numeralString = std::to_string(i+1);
+
+		if (i+1 < 10){
+			numeralString = "0" + numeralString;
+		}
+
+		std::string soundStartString = "vo/aperture_ai/escape_02_sphere_";
+		//std::string coreNameString = "curiosity";
+		std::string dash = "-";
+		//number
+		std::string soundEndString = ".wav";
+
+		std::string finalSoundName = soundStartString + coreNameString + dash + numeralString + soundEndString;
+
+		voiceLineDurations[i] = enginesound->GetSoundDuration(finalSoundName.c_str());
+		//DevMsg(finalSoundName.c_str());
+		//DevMsg("\n");
+	}
+}
+
+void CPropGladosCore::playCoreVoiceLine(std::string coreNameString, int lineNum, bool leadingZero){
+	std::string numeralString = std::to_string(lineNum+1);
+
+	if (lineNum+1 < 10 && leadingZero){
+		numeralString = "0" + numeralString;
+	}
+
+	std::string soundStartString = "Portal.Glados_core.";
+	//std::string coreNameString = "curiosity";
+	std::string dash = "_";
+	//number
+
+	std::string finalSoundName = soundStartString + coreNameString + dash + numeralString;
+
+	DevMsg("playing glados core sound: ");
+	DevMsg(finalSoundName.c_str());
+	DevMsg("\n");
+
+	EmitSound(finalSoundName.c_str());
+}
+
+void CPropGladosCore::precacheCoreVoiceLines(std::string coreNameString, int lineNum, bool leadingZero){
+	for (int i = 0; i < lineNum; i++){
+		std::string numeralString = std::to_string(i + 1);
+
+		if (i + 1 < 10 && leadingZero){
+			numeralString = std::string("0") + numeralString;
+		}
+
+		std::string soundStartString = "Portal.Glados_core.";
+		//std::string coreNameString = "curiosity";
+		std::string dash = "_";
+		//number
+
+		std::string finalSoundName = soundStartString + coreNameString + dash + numeralString;
+
+		//DevMsg("precaching glados core sound: ");
+		//DevMsg(finalSoundName.c_str());
+		//DevMsg("\n");
+
+		PrecacheScriptSound(finalSoundName.c_str());
+	}
 }
--- sp/src/game/server/prop_glados_core.h
+++ sp/src/game/server/prop_glados_core.h
@@ -1,4 +1,14 @@
+#include <string>
+
 #define CORE_MODEL_NAME "models/props_bts/glados_ball_reference.mdl"
+
+/**
+*	Core Type defintions
+*/
+#define	CORETYPE_CURIOUSITY 0		//The Curiousity Core.
+#define	CORETYPE_ANGER 1			//The Anger Core.
+#define	CORETYPE_CAKE 2				//The Cake/logic Core.
+#define	CORETYPE_MORALITY 3			//The Morality Core.
 
 /**
 *	Skin defintions
@@ -7,6 +17,17 @@
 #define	CORESKIN_CURIOUSITY 1		//The Curiousity Core Skin.
 #define	CORESKIN_ANGER 2			//The Anger Core Skin.
 #define	CORESKIN_CAKE 3				//The Cake/logic Core Skin.
+
+/**
+*	Core sound name definitions
+*/
+#define CORE_SOUNDSCRIPTNAME_CURIOUSITY "Curiosity"
+#define CORE_SOUNDSCRIPTNAME_ANGER "Aggressive"
+#define CORE_SOUNDSCRIPTNAME_CAKE "Crazy"
+
+#define CORE_SOUNDFILENAME_CURIOUSITY "curiosity"
+#define CORE_SOUNDFILENAME_ANGER "anger"
+#define CORE_SOUNDFILENAME_CAKE "cakemix"
 
 class CPropGladosCore : public CDynamicProp
 {
@@ -19,9 +40,28 @@ private:
 	void Precache(void);
 
 	void Think(void);
+	void AnimThink(void);
+	void OnPhysGunPickup(CBasePlayer *pPhysGunUser, PhysGunPickup_t reason);
 
-	int m_speechDelay;
+	void setupCoreVoiceLineDurations(float [], std::string, int);
+	void playCoreVoiceLine(std::string, int, bool);
+	void precacheCoreVoiceLines(std::string, int, bool);
+
+	float m_speechDelay;
 	int m_CoreType;
+
+	float m_lastVoiceLineTime;
+	float m_voiceLineDuration;
+
+	int m_voiceLineNumber;
+
+	bool m_isActive;
+	bool m_isTurning;
+	bool m_animJustEnded;
+
+	float voiceLineDurationsCurious[18];
+	float voiceLineDurationsAnger[21];
+	float voiceLineDurationsCake[41];
 
 	DECLARE_DATADESC();
 };
